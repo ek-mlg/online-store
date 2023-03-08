2a6c4d80dfca74c909d2a947d72efc2c3df2a76b
ek-mlg <ek.mlg@icloud.com>
2023-03-08 16:08:31 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <h1>Test тоолтлотлт</h1>
         <p>1</p>
         <p>2</p>
         <p>3</p>
         <p>4</p>
+
     </div>
   );
 }
